255dfb92e04cb614260bec98379247da3ed4e0fb
mohamedhassan66 <mohamed.hassan@datascience.me>
2026-01-05 15:53:23 +0200

    Revert "#ٍperformance and ui issues"
    This reverts commit 378d205053b82a7856735caabbba7a920f497388.

--- agentic_flow/scripts/dummy_output.py
+++ agentic_flow/scripts/dummy_output.py
@@ -1,11 +1,7 @@
 import time
 
 def generate_response(human_message: str, session_key: dict):
-<<<<<<< HEAD
     # time.sleep(1) # Reduced delay for testing
-=======
-    time.sleep(120)
->>>>>>> c246036aed8941c43a9b1565584aabf71651f10a
     for i in range(600):
         if i%20 == 0:
             yield str(' ')
